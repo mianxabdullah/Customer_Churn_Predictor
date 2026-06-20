87ee635511706c771a351cd6adc387aeb36f2480
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-06-20 12:23:53 +0500

    scaled numeric columns of cleaned data

--- scripts/train.py
+++ scripts/train.py
@@ -6,4 +6,8 @@ X = df.drop(columns=['Churn'])
 y = df['Churn']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42,stratify=y)
 
-
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+numeric_cols = ['tenure','MonthlyCharges','TotalCharges']
+X_train[numeric_cols]=scaler.fit_transform(X_train[numeric_cols])
+X_test[numeric_cols]=scaler.transform(X_test[numeric_cols])
